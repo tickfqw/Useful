1f48292dfc9a5af1adcaf6a620313b1b4f4ad3d1
tickfqw <919876902@qq.com>
2016-07-11 13:04:45 +0800

    Successful to use the gaode map.

--- app/src/main/java/com/example/tick/test/MainActivity.java
+++ app/src/main/java/com/example/tick/test/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.tick.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.show);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, showmap.class);
+                startActivity(intent);
+            }
+        });
     }
 }
